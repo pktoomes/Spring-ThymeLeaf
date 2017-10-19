4cac7be497d891049d31b5d3672db7a20b1b2010
Patrick Toomes <pktoomes@g.cofc.edu>
2017-10-19 05:49:05 -0400

    initial commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.awt.*;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private Grade grade;
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
